71a655e92332abe65d4f2561fd36f38cba8c5f78
Nehan Ahmed <nehanahmed2k23@gmail.com>
2025-12-15 13:55:19 +0500

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import HomeModal from "@/components/Home/HomeModal";
-import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Page() {
